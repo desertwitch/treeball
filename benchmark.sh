2c3af902c53a91e102c3bb476c4fe11d78929c89
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-09-19 09:08:32 +0200

    changes

--- benchmark.sh
+++ benchmark.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-read -pr "Enter TREEBALL_BIN path [./treeball]: " input
+read -p -r "Enter TREEBALL_BIN path [./treeball]: " input
 TREEBALL_BIN="${input:-./treeball}"
 
-read -pr "Enter RESULTS file path [./treeball_benchmark.txt]: " input
+read -p -r "Enter RESULTS file path [./treeball_benchmark.txt]: " input
 RESULTS="${input:-./treeball_benchmark.txt}"
 
-read -pr "Enter TMP_LOG file path [./treeball_benchmark_tmp.txt]: " input
+read -p -r "Enter TMP_LOG file path [./treeball_benchmark_tmp.txt]: " input
 TMP_LOG="${input:-./treeball_benchmark_tmp.txt}"
 
-read -pr "Enter BENCH_DIR path [./treeball_benchmark]: " input
+read -p -r "Enter BENCH_DIR path [./treeball_benchmark]: " input
 BENCH_DIR="${input:-./treeball_benchmark}"
 
-read -pr "Enter TMP_DIR path [./treeball_benchmark_tmp]: " input
+read -p -r "Enter TMP_DIR path [./treeball_benchmark_tmp]: " input
 TMP_DIR="${input:-./treeball_benchmark_tmp}"
 
-read -pr "Enter SIZES (space-separated) [5000 10000 50000 100000 500000 1000000 5000000]: " input
+read -p -r "Enter SIZES (space-separated) [5000 10000 50000 100000 500000 1000000 5000000]: " input
 SIZES=(${input:-5000 10000 50000 100000 500000 1000000 5000000})
 
 mkdir -p "$BENCH_DIR"
